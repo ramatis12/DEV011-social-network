757005d61e61e8781633ca4ef21e0e128f86dbf5
greysmpich <greys.mpich@gmail.com>
2023-10-09 13:50:35 -0600

    Se crearon dinámicamente los elementos de la página de bienvenida. Se añadieron archivos js (welcome, firebase, auth)

---
+++ src/auth.js
@@ -0,0 +1,16 @@
+// const auth = firebase.auth();
+
+// const provider = new firebase.auth.GoogleAuth();
+
+// export async function login() {
+//   try {
+//     const response = await auth.signInWithPopup(provider);
+//     return response.user;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// }
+
+// export function logout() {
+//   auth.signOut();
+// }
