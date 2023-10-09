c050567e42d2ad1d8566f2c4a481965c16fc5d6c
greysmpich <greys.mpich@gmail.com>
2023-10-09 14:35:56 -0600

    Revert "Se crearon dinámicamente los elementos de la página de bienvenida. Se añadieron archivos js (welcome, firebase, auth)"
    This reverts commit 757005d61e61e8781633ca4ef21e0e128f86dbf5.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,5 @@
 // Este es el punto de entrada de tu aplicacion
 
-// import { myFunction } from './lib/index.js';
-import { renderWelcome } from './welcome.js';
-import { login, logout } from './auth.js';
+import { myFunction } from './lib/index.js';
 
-const root = document.querySelector('#root');
-// const [inputUser, inputPwd, loginButton, registerButton] = renderWelcome();
-
-// root.appendChild(inputUser);
-// root.appendChild(inputPwd);
-// root.appendChild(loginButton);
-// root.appendChild(registerButton);
-
-root.appendChild(renderWelcome());
-
-const buttonLogin = document.querySelector('.login-button');
-
-buttonLogin.addEventListener('click', async (e) => {
-  try {
-    await login();
-  } catch (error){}
-});
-
-// root.appendChild
-// myFunction();
+myFunction();
