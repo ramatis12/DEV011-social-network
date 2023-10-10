e5e9bd334c0687fb14a1a0603558620966ae9530
Brigitte Utria <brigittekari@hotmail.com>
2023-10-10 11:55:39 -0500

    Cambios

---
+++ src/welcome.js
@@ -0,0 +1,8 @@
+export const renderWelcome = () => {
+    const inputUser = document.createElement("input");
+    inputUser.setAttribute("type","email");
+    inputUser.setAttribute("class" , "input-user");
+
+    const inputPwd = document.createElement("input");
+    inputPwd.setAttribute("type" , "password");
+}
