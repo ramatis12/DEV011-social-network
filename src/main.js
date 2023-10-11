870be3aac83169a36867093b4b1b7b4d9f91bfcd
SARA RAMIREZ <sara_ramatis@hotmail.com>
2023-10-10 22:18:13 -0600

    prueba de conexion de autentificacion

--- src/main.js
+++ src/main.js
@@ -3,25 +3,52 @@
 // import { myFunction } from './lib/index.js';
 import { renderWelcome } from './welcome.js';
 import { db } from "./firebase.js";
-import { createUserWithEmailAndPassword, auth } from './auth.js';
+//import { createUserWithEmailAndPassword, auth } from './auth.js';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 console.log(db);
 
 const root = document.querySelector('#root');
-// const [inputUser, inputPwd, loginButton, registerButton] = renderWelcome();
-
-// root.appendChild(inputUser);
-// root.appendChild(inputPwd);
-// root.appendChild(loginButton);
-// root.appendChild(registerButton);
-
 root.appendChild(renderWelcome());
+
+// const email = document.querySelector(".input-user");
+// const emailInput = email.value;
+// console.log (emailInput);
+
+
+
 
 const buttonLogin = document.querySelector('.login-button');
 
-buttonLogin.addEventListener('submit', (e) => {
-  
-  createUserWithEmailAndPassword()
+buttonLogin.addEventListener('click', (e) => {
+
+  const auth = getAuth();
+  console.log (getAuth());
+
+const email = document.querySelector(".input-user").value;
+ console.log (email);
+
+const password = document.querySelector('.input-pwd').value; 
+console.log (password);
+
+createUserWithEmailAndPassword(auth, email, password)
+.then((currentUser) => {
+        const user = currentUser.user;
+        alert("Usuario creado" + user);
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        
+        if(errorCode == 'auth/email-already-in-use')
+            alert('El correo ya está en uso');
+        else if (errorCode == 'auth/invalid-email')
+            alert ('El correo no es válido');
+        else if (errorCode == 'auth/weak-password')
+            alert ('La contraseña debe tener al menos 6 caracteres');
+
+    })
+
+  //createUserWithEmailAndPassword()
 });
 
 // root.appendChild
