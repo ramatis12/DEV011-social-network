12a13d75b5064af3cbcd345df3a14c43deb55bcb
Sara Ramirez <sara_ramatis@hotmail.com>
2023-10-12 10:48:04 -0600

    de nuevo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
 
 // import { myFunction } from './lib/index.js';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { renderWelcome } from './welcome.js';
 import { db } from "./firebase.js";
 //import { createUserWithEmailAndPassword, auth } from './auth.js';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 console.log(db);
 
@@ -15,13 +15,9 @@ root.appendChild(renderWelcome());
 // const emailInput = email.value;
 // console.log (emailInput);
 
-
-
-
 const buttonLogin = document.querySelector('.login-button');
 
 buttonLogin.addEventListener('click', (e) => {
-
   const auth = getAuth();
   console.log (getAuth());
 
@@ -32,20 +28,20 @@ const password = document.querySelector('.input-pwd').value;
 console.log (password);
 
 createUserWithEmailAndPassword(auth, email, password)
-.then((userCredential) => {
-        const user = userCredential.user;
-        alert("Usuario creado" + user);
+.then((currentUser) => {
+        const user = currentUser.user;
+        alert(`Usuario creado${user}`);
     })
     .catch((error) => {
         const errorCode = error.code;
         
-        if(errorCode == 'auth/email-already-in-use')
-            alert('El correo ya está en uso');
-        else if (errorCode == 'auth/invalid-email')
-            alert ('El correo no es válido');
-        else if (errorCode == 'auth/weak-password')
-            alert ('La contraseña debe tener al menos 6 caracteres');
-
+        if (errorCode === 'auth/email-already-in-use') {
+          alert('El correo ya está en uso');
+        } else if (errorCode === 'auth/invalid-email') {
+          alert('El correo no es válido');
+        } else if (errorCode === 'auth/weak-password') {
+          alert('La contraseña debe tener al menos 6 caracteres');
+        }
     })
 
   //createUserWithEmailAndPassword()
