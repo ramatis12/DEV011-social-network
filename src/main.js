c8dd91b4fcb179f01e847bfc06e6af3644357ca4
Sara Ramirez <sara_ramatis@hotmail.com>
2023-10-13 14:57:18 -0600

    autentificacion con Google

--- src/main.js
+++ src/main.js
@@ -1,32 +1,23 @@
 // Este es el punto de entrada de tu aplicacion
-
-// import { myFunction } from './lib/index.js';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { renderWelcome } from './welcome.js';
-import { db } from './firebase.js';
-// import { createUserWithEmailAndPassword, auth } from './auth.js';
+//import { renderRegister } from './register.js';
+//import { db } from './firebase.js';
 
-console.log(db);
 
+//------------------- Selectores Dom---------------------
 const root = document.querySelector('#root');
 root.appendChild(renderWelcome());
-
-// const email = document.querySelector(".input-user");
-// const emailInput = email.value;
-// console.log (emailInput);
-
 const buttonLogin = document.querySelector('.login-button');
+const googleButton = document.querySelector('.google-register-button');
+const registerButtonWelcome = document.querySelector('.register-button');
 
+
+//------------------ Autentificacion con correo --------------------------------------------
 buttonLogin.addEventListener('click', (e) => {
   const auth = getAuth();
-  console.log(getAuth());
-
   const email = document.querySelector('.input-user').value;
-  console.log(email);
-
   const password = document.querySelector('.input-pwd').value;
-  console.log(password);
-
   createUserWithEmailAndPassword(auth, email, password)
     .then((currentUser) => {
       const user = currentUser.user;
@@ -34,21 +25,34 @@ buttonLogin.addEventListener('click', (e) => {
     })
     .catch((error) => {
         const errorCode = error.code;
-        
-        const errorMessage = error.message;
-        
-        
+        const errorMessage = error.message;      
         // if(errorCode == 'auth/email-already-in-use')
         //     alert('El correo ya está en uso');
         // else if (errorCode == 'auth/invalid-email')
         //     alert ('El correo no es válido');
         // else if (errorCode == 'auth/weak-password')
-        //     alert ('La contraseña debe tener al menos 6 caracteres');
-
+        //     alert ('La contraseña debe tener al menos 6 caracteres')
     })
-
-  // createUserWithEmailAndPassword()
 });
 
-// root.appendChild
-// myFunction();
+//--------------------------- Autentificacion con Google-------------------------------------------------
+const googleProvider = new GoogleAuthProvider();
+googleButton.addEventListener('click', e =>{
+  const auth = getAuth();
+signInWithPopup(auth, googleProvider)
+.then((result) => {
+  // El usuario ha iniciado sesión con Google exitosamente.
+  const user = result.user;
+  console.log('Usuario autenticado:', user);
+})
+.catch((error) => {
+  // Ocurrió un error durante el proceso de inicio de sesión.
+  console.error('Error de autenticación con Google:', error);
+});
+});
+
+//--------------------- Boton para registrar un nuevo usuario-----------------------------------------
+registerButtonWelcome.addEventListener('click', (e) => {
+  root.innerHTML = '';
+  root.appendChild(renderRegister());
+});
