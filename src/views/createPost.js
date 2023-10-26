81320dd97f87f836dfd8b98ead1d2c1eb7431759
Brigitte Utria <brigittekari@hotmail.com>
2023-10-25 22:28:46 -0500

    verificar contraseña

--- src/views/createPost.js
+++ src/views/createPost.js
@@ -4,26 +4,31 @@ export const createPost = () => {
   const divCreatePost = document.createElement('div');
   divCreatePost.setAttribute('id', 'div-create-post');
 
+  const form = document.createElement ('form');
+  divCreatePost.appendChild(form);
+
   const urlImg = document.createElement('input');
+  urlImg.setAttribute('required', '');
   urlImg.setAttribute('id', 'url-img');
   urlImg.setAttribute('type', 'text');
   urlImg.setAttribute('placeholder', 'URL de tu foto');
   urlImg.value = '';
-  divCreatePost.appendChild(urlImg);
+  form.appendChild(urlImg);
 
   const textArea = document.createElement('textarea');
   textArea.setAttribute('id', 'text-area');
   textArea.setAttribute('placeholder', 'Escribe en 140 palabras un encabezado para tu foto:');
   textArea.setAttribute('maxlength', '140');
   textArea.value = '';
-  divCreatePost.appendChild(textArea);
+  form.appendChild(textArea);
 
   const buttonSave = document.createElement('button');
   buttonSave.setAttribute('id', 'button-save');
   buttonSave.textContent = 'Guardar';
-  divCreatePost.appendChild(buttonSave);
+  form.appendChild(buttonSave);
 
-  buttonSave.addEventListener('click', () => {
+  buttonSave.addEventListener('click', (e) => {
+    e.preventDefault();
     const text = divCreatePost.querySelector('#text-area');
     const imagen = divCreatePost.querySelector('#url-img');
     addPost(text.value, imagen.value);
