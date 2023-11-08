a68148848c4d0e7468e5544b6f496a6a6fcf078f
greysmpich <greys.mpich@gmail.com>
2023-11-08 11:48:01 -0600

    Footer, ya no cubre las actions del último post

--- docs/views/error.js
+++
@@ -1,8 +0,0 @@
-export const renderError = () => {
-  console.log(window.location.pathname);
-  const errorTitle = document.createElement('h2');
-  errorTitle.setAttribute('id', 'error-title');
-  errorTitle.textContent = '¡Santos mishis! El ratón que estabas siguiendo ha escapado. Error 404';
-
-  return errorTitle;
-};
